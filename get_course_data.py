e907532048bf128d045b59d8f3e7c2651c479e90
W1ndys <w1ndys@outlook.com>
2025-02-13 00:38:07 +0800

    重构：更新课程数据获取函数 重命名函数以更好地反映其用途 更新变量名以提高清晰度 修改 JSON 文件路径使用更通用的名称 改进日志记录和错误处理

--- get_course_data.py
+++ get_course_data.py
@@ -3,29 +3,33 @@ import logging
 import json
 
 
-def get_xsxkGgxxkxk_course_info(course):
+def get_course_jx02id_and_jx0404id(course):
+    """通过本地文件获取课程的jx02id和jx0404id"""
     try:
-        xsxkGgxxkxk_json_path = os.path.join(
-            os.path.dirname(__file__), "course_data", "xsxkGgxxkxk_course_info.json"
+        all_courses_json_path = os.path.join(
+            os.path.dirname(__file__), "course_data", "all_courses.json"
         )
-        with open(xsxkGgxxkxk_json_path, "r", encoding="utf-8") as file:
-            xsxkGgxxkxk_json_data = json.load(file)
-        for course_item in xsxkGgxxkxk_json_data["aaData"]:
+        with open(all_courses_json_path, "r", encoding="utf-8") as file:
+            all_courses_json_data = json.load(file)
+        for course_item in all_courses_json_data["aaData"]:
             if (
                 course_item["kch"] == course["course_id_or_name"]
                 and course_item["skls"] == course["teacher_name"]
                 and course_item["sksj"].replace("&nbsp;", "") == course["course_time"]
             ):
-                course_data = {
+                course_jx02id_and_jx0404id = {
                     "jx02id": course_item["jx02id"],
                     "jx0404id": course_item["jx0404id"],
                 }
 
-                if course_data["jx02id"] and course_data["jx0404id"]:
+                if (
+                    course_jx02id_and_jx0404id["jx02id"]
+                    and course_jx02id_and_jx0404id["jx0404id"]
+                ):
                     logging.info(
-                        f"找到课程: {course['course_id_or_name']}的jx02id: {course_data['jx02id']}, jx0404id: {course_data['jx0404id']}"
+                        f"找到课程: {course['course_id_or_name']}的jx02id: {course_jx02id_and_jx0404id['jx02id']}, jx0404id: {course_jx02id_and_jx0404id['jx0404id']}"
                     )
-                    return course_data
+                    return course_jx02id_and_jx0404id
 
     except Exception as e:
         logging.error(f"获取公选课选课页面数据失败: {e}")
